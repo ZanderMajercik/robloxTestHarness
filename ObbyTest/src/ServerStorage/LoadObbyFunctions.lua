e1a525bf28dd620bd11fd41834232d59cc93f615
Sharon Zhang <sharonzhang@roblox.com>
2024-06-27 14:01:44 -0700

    load script

--- ObbyTest/src/ServerStorage/LoadObbyFunctions.lua
+++ ObbyTest/src/ServerStorage/LoadObbyFunctions.lua
@@ -2,38 +2,59 @@ local LoadObbyFunctions = {}
 
 local HttpService = game:GetService("HttpService")
 local CollectionService = game:GetService("CollectionService")
-local TagFunctions = require(game:GetService("ServerStorage").TagFunctions)
+local TagFunctions = require(game:GetService("ReplicatedStorage").TagFunctions)
 
 local function createBlockPart(blockPartName, blockPartInfo)
-    -- Type is almost always "Part". "SpawnLocation" is the only special case.
     local part = Instance.new(blockPartInfo["type"])
     part.Name = blockPartName
     part.Anchored = true
     part.Shape = Enum.PartType.Block
-    part.Color = Color3.new(0.5, 0.5, 0.5) -- Special color will be set by TagFunctions if applicable.
-
+    print(blockPartInfo["tags"])
     if blockPartInfo["type"] == "SpawnLocation" then
         part.Color = Color3.new(0, 1, 0)
+    elseif blockPartInfo["tags"][1] == "KillBlock" then
+        part.Color = Color3.new(1, 0, 0)
+    elseif blockPartInfo["tags"][1] == "Goal" then
+        part.Color = Color3.new(0,0,1)
+    else
+        part.Color = Color3.new(0.5, 0.5, 0.5)
     end
-
     for idx, t in blockPartInfo["tags"] do
-        -- Tags are used for both agent observations and game logic.
         CollectionService:AddTag(part, t)
-        
-        -- Associate the touched event with the associate tag function 
-        -- (if there is one) and set the color for visualization.
-        if TagFunctions[t] then
+        if t == "KillBlock" then
             TagFunctions[t](part)
         end
     end
-    -- NOTE: The JSON files store (X, Y, Z). The vertical axis in Roblox is the Y-axis.
-    part.Position = Vector3.new(blockPartInfo["position"][1], blockPartInfo["position"][3], blockPartInfo["position"][2])
-    part.Size = Vector3.new(blockPartInfo["size"][1], blockPartInfo["size"][3], blockPartInfo["size"][2])
-
     part.Parent = workspace -- Put the part into the Workspace
+
+    -- NOTE: The JSON files store (X, Y, Z). The vertical axis in Roblox is the Y-axis.
+    part.Position = Vector3.new(blockPartInfo["position"][1], -blockPartInfo["position"][3], blockPartInfo["position"][2])
+    part.Size = Vector3.new(blockPartInfo["size"][1], blockPartInfo["size"][3], blockPartInfo["size"][2])
 end
 
-function LoadObbyFunctions.createObbyFromJson(data)
+local function parseObbyJSON(json_url)
+    response = HttpService:GetAsync(json_url)
+    data = HttpService:JSONDecode(response)
+    -- Parses blocks from obby JSON
+    for blockPartName, blockPartInfo in pairs(data) do
+        createBlockPart(blockPartName, blockPartInfo)
+    end
+end
+
+function LoadObbyFunctions.createObbyFromJson(url, obbyName)
+    local jsonFilename = string.format('%s.json', obbyName)
+    local response = HttpService:RequestAsync({
+        Url = url, -- This website helps debug HTTP requests
+        Method = "POST",
+        Headers = {
+            ["Content-Type"] = "application/json", -- When sending JSON, set this!
+        },
+        Body = HttpService:JSONEncode({ filename = jsonFilename }),
+    })
+    print(response)
+    -- data = HttpService:JSONDecode(response)
+    data = HttpService:JSONDecode(response["Body"])
+    print(data)
     -- Parses blocks from obby JSON
     for blockPartName, blockPartInfo in pairs(data) do
         createBlockPart(blockPartName, blockPartInfo)
